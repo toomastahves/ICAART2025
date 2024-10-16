a54c8f82c9af68f37e8060a29f564fb48eb3b501
Toomas Tahves <toomastahves@hotmail.com>
2024-10-16 22:16:13 +0300

    fix file cleanup

--- utils/delete_files.py
+++ utils/delete_files.py
@@ -14,8 +14,11 @@ with open(config_file, 'r') as f:
     config = json.load(f)
 
 files = glob.glob(config['Log']['logdir']+'progress_save/*.pth')
-latest_file = max(files, key=os.path.getctime)
-for file in files:
-    if not file != latest_file:
-        os.remove(file)
-        print(f'Removed: {file}')
+
+if len(files) > 1:
+    latest_file = max(files, key=os.path.getctime)
+    print(f'Latest file: {latest_file}')
+    for file in files:
+        if file != latest_file:
+            os.remove(file)
+            print(f'Removed: {file}')
